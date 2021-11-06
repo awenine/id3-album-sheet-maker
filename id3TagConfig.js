894cf366ed8a12ebe1b9f9fcb3ccfe7f4548d2c7
Chris Owen <c.ornine@gmail.com>
2021-11-07 01:35:15 +0800

    create and annotate id3ConfigFile

---
+++ id3TagConfig.js
@@ -0,0 +1,11 @@
+//* modify this object and pass to 'returnID3Tags' function to define shape of id3 data
+// for full glossary see: https://www.npmjs.com/package/node-id3
+
+const id3TagConfig = {
+  // include: ['TALB', 'TIT2'],    // only read the specified tags (default: all)
+  exclude: ['APIC'],            // don't read the specified tags (default: [])
+  // onlyRaw: false,               // only return raw object (default: false)
+  noRaw: true                  // don't generate raw object (default: false)
+}
+
+module.exports = id3TagConfig
