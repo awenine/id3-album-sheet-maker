4606c4782091f4d1b63c7cad47590a394dc89fc6
Chris Owen <c.ornine@gmail.com>
2021-11-07 02:34:21 +0800

    add google sheets authorization boilerplate

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
+const authorizeAndRunSheetScript = require('./authorizeAndRunSheetScript');
 const getNestedMp3s = require('./getNestedMp3s');
 const id3TagConfig = require('./id3TagConfig');
 const returnID3Tags = require('./returnID3Tags');
 
-const mp3Folders = getNestedMp3s(process.env.MP3_ROOT_FOLDER)
-  .slice(0,5)
-  .map(paths => returnID3Tags(paths, id3TagConfig));
+const id3TagFolders = 
+  getNestedMp3s(process.env.MP3_ROOT_FOLDER)
+    .map(paths => returnID3Tags(paths, id3TagConfig));
 
-console.log(mp3Folders);
+authorizeAndRunSheetScript(console.log) // to test
