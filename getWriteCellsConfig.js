8d756293e01b6bfd10c28e574ddbfd058abe83ad
Chris Owen <c.ornine@gmail.com>
2021-11-07 23:49:01 +0800

    add write cells formatting configuration

---
+++ getWriteCellsConfig.js
@@ -0,0 +1,30 @@
+function getWriteCellsConfig(releaseData) {
+  const releaseFormat = [  
+    [ // Column A
+      'Artist',
+      'Album',
+      'Year',
+      'Genre',
+      '',
+      'Tracklist:'
+      // after will come track numbers
+    ],
+    [ // Column B
+      // will be filled according to release data
+    ]
+  ]
+
+  releaseData.forEach((trackData, index) => {
+    releaseFormat[0].push(trackData.trackNumber)
+    if (index === 0) releaseFormat[1].push(trackData.artist,trackData.album,trackData.year,trackData.genre,'','');
+    releaseFormat[1].push(trackData.title)
+  });
+
+  return {
+      range: `${releaseData[0].comment.text}!`,
+      majorDimension = 'COLUMNS',
+      values: releaseFormat
+  }
+}
+
+module.exports = getWriteCellsConfig
