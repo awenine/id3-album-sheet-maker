d420ce5819838752dde90570c0c17fadf0dada12
Chris Owen <c.ornine@gmail.com>
2022-03-22 22:34:54 +0000

    chore: add notes on albums unable to log

--- index.js
+++ index.js
@@ -10,12 +10,18 @@ const getQNWriteCellsConfig = require('./getQNWriteCellsConfig');
 
 const id3TagFolders = 
   getNestedMp3s(process.env.MP3_ROOT_FOLDER)
-    .slice(1)   //* for getting a smaller range of folders
+    .slice(57)   //* for getting a smaller range of folders
     .map(paths => {
       const tags = returnMp3Data(paths, id3TagConfig)
       log(`${paths[0].split('/')[4]} id3 tags fetched`)
       return tags
     })
+//! Albums with issues (may need manual input):
+// QNAPP001 (dane law app)
+// QNR034 (silent night)
+// QNREC002
+// Singles (same cat as records) - QNS001, QNS002 
+
 
 authorizeAndRunSheetScript(createSheets,id3TagFolders,getSheetConfig, getQNWriteCellsConfig)
 
